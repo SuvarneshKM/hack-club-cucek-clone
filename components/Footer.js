72e1f037c4984c91f3ec8b790a21dccb6b051209
Suvarnesh K M <suvarnesh1729@gmail.com>
2021-09-15 22:36:41 +0530

    footer

--- components/Footer.js
+++ components/Footer.js
@@ -1,7 +1,35 @@
 function Footer() {
     return (
-        <section>
-            <h1>Footer</h1>
+        <section className="bg-dark max-w-[1236px] mx-auto lg:pt-[120px] pt-[80px] px-[16px] py-[16px]">
+            <div className="flex flex-col lg:flex-row">
+                <div className="">
+                    <h1 className="text-red lg:text-[22px] text-[20px] font-semibold">Hack Club CUCEK</h1>
+                    <ul className="lg:text-[18px] text-[16px] font-normal lg:pt-[32px] pt-[24px] flex lg:flex-col flex-row justify-center items-center lg:text-left">
+                        <li>Events</li>
+                        <li>Our Team</li>
+                        <li>Join Us</li>
+                        <li>Scrapbook</li>
+                        <li>Gallery</li>
+                    </ul>
+                </div>
+                <div>
+                    <h1 className="text-red lg:text-[22px] text-[20px] font-semibold">Address</h1>
+                    <p>
+                        Cochin University College of Engineering Kuttanad(CUCEK), Pulincunnu P.O,
+                        Alleppey, 688504, Kerala
+                    </p>
+                    <p>
+                        All Rights Reserved 2021, Hack Club CUCEK
+                    </p>
+                </div>
+                <div>
+                    <h1 className="text-red lg:text-[22px] text-[20px] font-semibold">Socials</h1>
+                    <div>
+
+                    </div>
+                    <i></i>
+                </div>
+            </div>
         </section>
     )
 }
